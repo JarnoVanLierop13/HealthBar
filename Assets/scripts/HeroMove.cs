973d944ae4c1f9f9bc22370ce4fe68c49becdee3
LAPTOP\Jarno van Lierop <jarnovanlierop@gmail.com>
2018-04-19 10:22:57 +0200

    first commmit

---
+++ Assets/scripts/HeroMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroMove : MonoBehaviour
+{
+
+    float dirX, dirY;
+    Rigidbody2D rb;
+    public float moveSpeed = 2f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        dirX = Input.GetAxis("Horizontal");
+        dirY = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(dirX * moveSpeed, dirY * moveSpeed);
+    }
+}
